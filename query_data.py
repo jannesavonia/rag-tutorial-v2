0455917ad2c55714ee97198e3ac4e5f040f6dd68
jannesavonia <74650276+jannesavonia@users.noreply.github.com>
2026-09-16 14:38:28 +0300

    Update query_data.py

--- query_data.py
+++ query_data.py
@@ -1,6 +1,6 @@
 import argparse
-from langchain.vectorstores.chroma import Chroma
-from langchain.prompts import ChatPromptTemplate
+from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 
 from get_embedding_function import get_embedding_function
@@ -38,7 +38,9 @@ def query_rag(query_text: str):
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    # print(prompt)
+    print(32*"-")
+    print(prompt)
+    print(32*"-")
 
     model = Ollama(model="mistral")
     response_text = model.invoke(prompt)
